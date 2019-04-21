2f8e5f7bbe79ce1f574c20e9f79bf82da85c7b70
Kapil Pagani <pagani@hpe.com>
2019-04-21 12:35:19 +0530

    Added Recipe component

--- src/pages/SingleRecipe.js
+++ src/pages/SingleRecipe.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
 export default class SingleRecipe extends Component {
+  constructor(props){
+    super(props);
+    console.log(this.props.match.params.id);
+    
+  }
+
   render() {
     return (
       <h4>
-        Hello from SingleRecipe
+        Hello from SingleRecipe {this.props.match.params.id}
       </h4>
     )
   }
