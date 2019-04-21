8ef4e89bb786714d1bf733aa8e6aa3902ae87021
Kapil Pagani <pagani@hpe.com>
2019-04-21 11:20:15 +0530

    Added RecipeList Component

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -3,11 +3,27 @@ import Recipe from '../components/Recipe'
 
 export default class RecipeList extends Component {
   render() {
+    const { recipes } = this.props;
     return (
-      <div>
-        Hello From RecipeList
-        <Recipe/>
-      </div>
+      <React.Fragment>
+        <div className="container py-5">
+          {/* Title */}
+          <div className="row">
+            <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
+              <h1 className="text-slanted">recipe List</h1>
+            </div>
+          </div>
+
+          <div className="row">
+            {recipes.map(recipe => {
+              return (
+                <Recipe key={recipe.recipe_id} recipe={recipe} />
+              )
+            })}
+          </div>
+
+        </div>
+      </React.Fragment>
     )
   }
 }
