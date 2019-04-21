b05559849a4b24f0204f6a72496cd8bc6e1e2d4a
Kapil Pagani <pagani@hpe.com>
2019-04-21 21:57:23 +0530

    Added AJAX call function

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -12,14 +12,21 @@ export default class Recipes extends Component {
   state = {
     recipes: recipeData,
     search: '',
-    url: `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`
+    url: `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`,
+    base_url: `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`,
+    query: '&q=',
+    error: ""
   }
 
   async getRecipes() {
     try {
       const data = await fetch(this.state.url);
       const jsonData = await data.json();
-      console.log(jsonData);
+      if (jsonData.recipes.length === 0) {
+        this.setState({
+          error: "sorry but your search did not return anything"
+        })
+      }
       this.setState({
         recipes: jsonData.recipes
       })
@@ -28,9 +35,9 @@ export default class Recipes extends Component {
     }
   }
 
-  async componentDidMount() {
-    this.getRecipes();
-  }
+  // async componentDidMount() {
+  //   this.getRecipes();
+  // }
 
   handleChange = (e) => {
     this.setState({
@@ -40,13 +47,28 @@ export default class Recipes extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
+    const {base_url, query, search} = this.state;
+    this.setState({
+      url : `${base_url}${query}${search}`,
+      search: ""
+    }, ()=> this.getRecipes())
   }
 
   render() {
     return (
       <React.Fragment>
         <Search handleChange={this.handleChange} handleSubmit={this.handleSubmit} search={this.state.search} />
-        <RecipeList recipes={this.state.recipes} />
+        {this.state.error ?
+          (<section>
+            <div className="row">
+              <div className="col">
+                <h2 className="text-orange text-center text-uppercase mt-5">
+                  {this.state.error}
+                </h2>
+              </div>
+            </div>
+          </section>) :
+          (<RecipeList recipes={this.state.recipes} />)}
       </React.Fragment>
     )
   }
