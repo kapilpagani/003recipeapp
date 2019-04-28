aeffe46fcecaa689f6d14d7138efe19228105948
Kapil Pagani <pagani@hpe.com>
2019-04-28 17:56:41 +0530

    updated Navbar color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand navbar-light bg-light font-weight-bold">
+        <nav className="navbar navbar-expand navbar-light bg-warning font-weight-bold">
             <Link to="/" className="navbar-brand text-slanted" id="im1" style={{ "marginRight": "60px" }}>Recipe App</Link>
             <div className="collapse navbar-collapse show">
                 <ul className="navbar-nav">
