7729158573c78b40ba942fc1b29b88d20b8991a2
Kapil Pagani <pagani@hpe.com>
2019-04-28 17:50:52 +0530

    Removed browserRouter

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,22 @@
 import React from 'react';
 // import logo from '../images/logo.svg';
-import { Link, BrowserRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
-        <BrowserRouter>
-            <nav className="navbar navbar-expand navbar-light bg-light font-weight-bold">
-                <Link to="/" className="navbar-brand text-slanted" id="im1" style={{ "marginRight": "60px" }}>Recipe App</Link>
-                <div className="collapse navbar-collapse show">
-                    <ul className="navbar-nav">
-                        <li className="navbar-item">
-                            <Link className="nav-link text-primary mx-2" to="/">Home</Link>
-                        </li>
-                        <li className="navbar-item">
-                            <Link className="nav-link text-primary mx-2" to="/recipes">Recipes</Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </BrowserRouter>
-
+        <nav className="navbar navbar-expand navbar-light bg-light font-weight-bold">
+            <Link to="/" className="navbar-brand text-slanted" id="im1" style={{ "marginRight": "60px" }}>Recipe App</Link>
+            <div className="collapse navbar-collapse show">
+                <ul className="navbar-nav">
+                    <li className="navbar-item">
+                        <Link className="nav-link text-primary mx-2" to="/">Home</Link>
+                    </li>
+                    <li className="navbar-item">
+                        <Link className="nav-link text-primary mx-2" to="/recipes">Recipes</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
         //     <div role="navigation">
         //     <div class="navbar navbar-inverse navbar-expand-sm navbar-light bg-light">
         //         <Link to="/" class="navbar-brand" id="im1">Recipe Application</Link>
